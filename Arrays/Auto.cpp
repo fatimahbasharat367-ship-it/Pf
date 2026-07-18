222e3b456659474ad69a2102375c2ed62e7e5f74
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-07-18 11:26:10 -0700

    Arrays

---
+++ Arrays/Auto.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char A[5] = {65, 66, 'C', 68, 69};
+
+    for ( auto x : A ){
+        cout << x << endl;
+    }
+   return 0;
+}
---
+++ Arrays/SizeOfArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A;
+    cout << "Enter the value of A :";
+    cin >> A;
+    int arr[A];
+
+    cout << sizeof arr << endl;
+
+   return 0;
+}
