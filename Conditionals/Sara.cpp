3d91d25a2fddcc72c166470cb9879a9971e5a07d
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-07-21 10:29:04 -0700

    Conditionals

---
+++ Conditionals/Sara.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string name;
+   cout << "Enter the name : ";
+    cin >> name;
+
+    if (name =="Sara"){
+        cout << "Bestie";
+    }
+    else{
+        cout << " Not mine bestie. ";
+    }
+   return 0;
+}
