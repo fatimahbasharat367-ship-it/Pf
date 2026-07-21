7577e449e04f8de423de56d2e0ee5a83a7f279b8
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-07-21 10:26:58 -0700

    Nested Loop

---
+++ Loops/LoopsNested.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int k =0;
+    for (int i =1;  i<5; i++ ){
+        for(int j = 1; j <=i; j++){
+      k++;
+            cout << k ;
+        }
+    
+        cout << endl;
+    }
+   return 0;
+}
