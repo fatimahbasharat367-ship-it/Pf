1a6329d5b740f56d6aaa077cb14292bf37992855
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-01 23:36:21 -0700

    Easy Task

---
+++ Basics/Easy2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter the value of a :";
+    cin >> a;
+
+    int b;
+    cout << " Enter the value of b :";
+    cin >> b;
+
+    int sum;
+    sum = a + b ;
+    cout << "The sum of two variables is :" << sum;
+   return 0;
+}
---
+++ Basics/Easy3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age ;
+    cout << "Enter how older are you ? ";
+    cin >> age ;
+    cout << "Your age is : " << age ;
+   return 0;
+}
