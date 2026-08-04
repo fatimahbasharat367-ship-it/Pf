a5972ce2ad04046cdbb5b8adfc57b7c680d9a728
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-04 05:02:49 -0700

    Basic

---
+++ Basics/hehe.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your Beautiful name :";
+    cin >> name;
+    float cgpa;
+    cout << "Enter the cgpa you attained :";
+    cin >> cgpa;
+   string program;
+   cout << "Enter your program in which you are studying :";
+   getline(cin, program);
+   return 0;
+}
