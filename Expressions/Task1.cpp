5f998072d5a574f650b0e99a00eb92e43f3965ac
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-05 09:47:11 -0700

    Tasks

---
+++ Expressions/Task1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int x = 8 + 9;
+    cout << " The sum of both nnumbers is : " << x ;
+   return 0;
+}
---
+++ Expressions/Task2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 12 ;
+    int z ;
+    z = ( x / 2 ) - 1;
+    cout << " The answer is : " << z;
+   return 0;
+}
