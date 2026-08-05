bad615acdcf0cca82168cc483b5ec150319a6026
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-05 09:52:45 -0700

    Tasks

---
+++ Expressions/Task3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    x = 6;
+    int y; 
+    y = 12;
+    int z;
+
+    z = x + 10 + y;
+
+    cout << " The sum of the above  expression is : " << z; 
+   return 0;
+}
---
+++ Expressions/Task4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // + ==> operator 
+    // x , y , z ==> operand
+    int p ;
+
+    int x = 10 ;
+    int y = 10;
+    int z = 10;
+
+    p = x + y + z;
+     
+    cout << " The sum of the above expression is : "  << p ;
+   return 0;
+}
