823039fd6ebb144c2fd2c9ba13b55d2401127698
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-05 09:58:03 -0700

    Tasks

---
+++ Expressions/Task5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int x = 8;
+    int y = 3;
+
+    int z ;
+   z = x - y + 10 ;
+
+   cout << " The result of the above expression is : " << z;
+   return 0;
+}
---
+++ Expressions/Task6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    x = 4 ;
+    int z;
+
+    z = x / 2 ;
+    cout << " The result of the above expression is : " << z;
+   return 0;
+}
