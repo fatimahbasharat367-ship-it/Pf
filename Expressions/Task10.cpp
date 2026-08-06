952f35d8768375eaa83f5943d922cc2ae1005d0d
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-06 01:09:03 -0700

    Tasks

---
+++ Expressions/Task10.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int s ;
+    s = (10 * 2) / 5 ;
+    cout << " The result of the above expression is :" << s;
+   return 0;
+}
---
+++ Expressions/Task9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int f;
+     f = 10 - ( 2 * 4 ) ;
+    // or
+    // f = 10 - ( 2 * 4 ) ;
+    cout << "The result of the above exprression is : " << f;
+   return 0;
+}
