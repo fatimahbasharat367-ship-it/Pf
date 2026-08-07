943d2607bfe5c4bb986d9c992217daad0b5800ff
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-07 03:50:56 -0700

    Chapter 2 ( Ustadam ) Problem

---
+++ Expressions/Challenge1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    // Half Pyramid:
+    
+    cout << " * " << endl;
+    cout << " * *" << endl;
+    cout << " * * *" << endl;
+    cout << " * * * *" << endl;
+    cout << " * * * * * " << endl;
+    cout << " * * * * * * " << endl;
+    cout << " * * * * * * * " << endl;
+    
+   return 0;
+}
