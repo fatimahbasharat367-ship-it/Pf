879c8cb5c5c4e35bc9fc1e5379614e0fc546a8f0
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-10 16:10:04 -0700

    Basics Easy Task

---
+++ Basics/Easy4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x , y , z;
+    
+    cout << " Enter x : " ;
+    cin >> x;
+    cout << " Enter y : ";
+     cin >> y;
+    z = x + y ;
+
+    cout << "sum is : " << z ;
+   return 0;
+}
