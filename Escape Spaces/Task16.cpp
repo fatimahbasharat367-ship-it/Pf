b42bee79728450e589fe60c6970d15a41ee2b41a
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-15 14:16:22 -0700

    Escape Space

---
+++ Escape Spaces/Task16.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << " \" Enter what do you want to buy. \" ";
+    cout << "\n Apple \t Banana\t PineAppple\tStrawberry \n 200/- \t 400/- \t 500/- \t\t 1000/-";
+    
+   return 0;
+}
