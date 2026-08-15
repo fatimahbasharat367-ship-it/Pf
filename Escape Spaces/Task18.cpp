ee831d1b86b3e52f80c4fe2e0f55fc429fb9e9a3
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-15 14:19:27 -0700

    Escape Space Task

---
+++ Escape Spaces/Task18.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Are you a Student \?";
+    cout << "\n1) Yes \t 2) No ";
+    cout << "\nIn which university do you read \? \n 1) UET \t2) COMSATS\t3) KE";
+   return 0;
+}
