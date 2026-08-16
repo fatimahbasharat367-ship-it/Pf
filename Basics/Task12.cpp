b629160be4c714309e86ddcb23d35e23bab20907
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-16 16:04:56 -0700

    Basic Task

---
+++ Basics/Task12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Year likho: ";
+    cin >> year;
+
+    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        cout << year << " is a Leap Year";
+    else
+        cout << year << " is not a Leap Year";
+    return 0;
+}
