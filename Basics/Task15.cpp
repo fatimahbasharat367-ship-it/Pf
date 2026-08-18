75331c7ef04d4aeee3b17c3eadc38475fab533b8
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-17 17:07:46 -0700

    Basic Task

---
+++ Basics/Task15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "\n***************\n";
+    cout << "Hello " << name << "\n";
+    cout << "Have a nice day!\n";
+    cout << "***************\n";
+    return 0;
+}
