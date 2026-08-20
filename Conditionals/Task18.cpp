06aa96cca9478391b72b663e25ec0978d601213e
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-19 19:25:16 -0700

    Conditional Task

---
+++ Conditionals/Task18.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float percentage;
+    float maths;
+    cout << " Enter your number in maths : " ;
+    cin >> maths;
+
+     float calculus;
+    cout << " Enter your number in calculus : " ;
+    cin >> calculus;
+
+     float programming;
+    cout << " Enter your number in programming : " ;
+    cin >> programming;
+
+    if (programming <= 40 || calculus <= 40 || maths <= 40 ){
+        cout << " You are Fail. Try Again next time. ";
+    }else{
+// else( programming > 40 && calculus && 40 || maths && 40){
+    percentage = ((programming + calculus + maths)* 300 )/100;
+}
+
+if (percentage >= 80){
+    cout << " You got A Grade. ";
+}else if ( percentage >= 60 ){
+    cout << " You got B Grade. ";
+}else if (percentage >= 40 ){
+    cout << " You got C Grade. ";
+}else 
+{
+    cout << " You got F Grade. ";
+}
+   return 0;
+}
