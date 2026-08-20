10d4efa7e6299c57a5238f6784408be7c2bb4edd
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-20 15:22:12 -0700

    Conditional Task

---
+++ Conditionals/Task20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num ;
+    cout << " Enter number : ";
+    cin >> num;
+    if (num > 0) {
+        cout << "Number positive hai." << endl;
+    }
+    return 0;
+}
