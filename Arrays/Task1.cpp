b855c4045bcbf0d1406ee9366bb089d6a8c157d6
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-21 13:32:52 -0700

    Arrays Task

---
+++ Arrays/Task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numbers[5];  
+
+    numbers[0] = 10;  
+    numbers[1] = 20;   
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    cout << numbers[0] << endl; 
+    cout << numbers[4] << endl; 
+      cout << numbers[1] << endl; 
+}
