2592256edf966885df7e51ffd98dd862a50f3ac1
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-21 13:33:25 -0700

    Arrays Task

---
+++ Arrays/Task2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num [7] = {3, 5, 8, 9, 13, 78, 89};
+
+    cout <<  " Number at index 0 is " << num [0] << endl;
+    cout <<  " Number at index 1 is " << num [1] << endl;
+    cout <<  " Number at index 2 is " << num [2] << endl;
+    cout <<  " Number at index 3 is " << num [3] << endl;
+    cout <<  " Number at index 4 is " << num [4] << endl;
+    cout <<  " Number at index 5 is " << num [5] << endl;
+    cout <<  " Number at index 6 is " << num [6] << endl;
+   return 0;
+}
