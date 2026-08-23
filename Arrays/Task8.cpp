37ed258128e0e87ead7e623683969ace0e2ddbe9
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-23 10:49:35 -0700

    Arrays Task

---
+++ Arrays/Task8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int scores[3] = {93, 96, 99} ;
+ 
+    cout << "Your Biology Number is : " << scores[0] << endl; 
+    cout << "Your Chemistry Number is : " << scores[1] << endl; 
+    cout << "Your Physics Number is : " << scores[2] << endl; 
+
+    return 0;
+}
