7ddd3d07fa3d66411852c29bd210777ce852b6c0
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-23 10:50:47 -0700

    Arrays Task

---
+++ Arrays/Task9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+    for(int i = 0; i < 5; i++) {
+        cout << vowels[i] << " ";
+    }
+    return 0;
+}
