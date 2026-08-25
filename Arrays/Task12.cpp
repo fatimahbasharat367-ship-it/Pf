8bc2708b11926127bc5494b2619387cf9d757536
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-24 21:29:17 -0700

    2D Arrays Task

---
+++ Arrays/Task12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3];
+    cout << "Enter 9 numbers:" << endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "Matrix:" << endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
