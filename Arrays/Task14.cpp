0ed771812b482db6abfa39bbc0f01ee4f231394f
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-24 21:55:17 -0700

    2D Arrays Task

---
+++ Arrays/Task14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[3][3], t[3][3];
+    cout << "Enter 3x3 matrix:" << endl;
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            cin >> a[i][j];
+
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            t[j][i] = a[i][j];
+
+    cout << "Transpose:" << endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+            cout << t[i][j] << " ";
+        cout << endl;
+    }
+}
