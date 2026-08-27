c0eaa9d0ca609267bd7fa52d2c6b1158c8046a45
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-26 21:54:55 -0700

    String Task

---
+++ Strings/Task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cout << " Enter the word : ";
+    cin >> word;
+    cout << "Word: " << word << endl;
+    cout << "Length: " << word.length() << endl;
+    return 0;
+}
