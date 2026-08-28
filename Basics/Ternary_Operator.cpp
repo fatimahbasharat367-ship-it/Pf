92b5943e5fdd80cd2fa1bfb338bfbc43dff056a5
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-27 20:33:21 -0700

    Basics

---
+++ Basics/Ternary_Operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int max = (a > b) ? a : b;
+    cout << "Greater number is: " << max;
+    return 0;
+}
