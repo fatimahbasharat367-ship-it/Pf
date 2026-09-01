fba8559270a7ad0459294b5ef8fd55c4316b8c93
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-08-31 18:39:38 -0700

    Expressions Challenge

---
+++ Expressions/Challenge14.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int sum=0;
+
+    // 1st
+    cout << " Enter the First Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    //  2nd
+    cout << " Enter the Second Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 3rd
+    cout << " Enter the Third Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 4
+    cout << " Enter the Fourth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 5
+    cout << " Enter the Fifth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 6
+    cout << " Enter the Sixth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 7
+    cout << " Enter the Seventh Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 8
+    cout << " Enter the Eighth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 9
+    cout << " Enter the Nineth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    // 10
+    cout << " Enter the Tenth Number : ";
+    cin >> num1;
+    if (num1 % 2 == 0)
+    {
+        sum += num1;
+    }
+
+    cout << " The Sum of the Even Numbers you added is : " << sum;
+
+    return 0;
+}
