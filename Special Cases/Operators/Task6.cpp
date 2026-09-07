13ad595ba6021b4b1f822b75dcf7b3449f8717ba
The-Fatixmah <fatimahbasharat367@gmail.com>
2026-09-06 23:20:54 -0700

    Operators Task

---
+++ Special Cases/Operators/Task6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4, b = 2;
+    int result = (a < b) ? a : b;
+
+    cout << "Greater number is: " << result << endl;
+    return 0;
+}
